1a621f2989c35fc166ee9df8b8ffd1d9c3bc37b6
Johan 't Hart <johanthart@gmail.com>
2019-07-19 00:17:47 +0200

    Connect  to beatbox channel

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello world 4');
+import * as WebSocket from 'ws';
+import { connectWampChannel, makeObservableWebSocket } from 'wamprx';
+import { switchMap } from 'rxjs/operators';
+
+console.log('Play around with beatbox on https://demo.crossbar.io/beatbox/index.html');
+
+const myMakeObsWs = makeObservableWebSocket(
+    (url, protocol) => new WebSocket(url, protocol));
+
+connectWampChannel('wss://demo.crossbar.io/ws', 'crossbardemo', undefined, myMakeObsWs).pipe(
+    switchMap(channel => channel.subscribe('io.crossbar.demo.beatbox.692497.pad_down')))
+    .subscribe(([,received]) => console.log('Received: ' + Object.keys(received!)));
