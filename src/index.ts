1e31d7f5e9452af8c464c6e4ee3c537656bdd761
Johan 't Hart <johanthart@gmail.com>
2019-07-19 00:29:15 +0200

    Also receive pad up events

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as WebSocket from 'ws';
 import { connectWampChannel, makeObservableWebSocket } from 'wamprx';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, map } from 'rxjs/operators';
+import { merge } from 'rxjs';
 
 console.log('Play around with beatbox on https://demo.crossbar.io/beatbox/index.html');
 
@@ -8,5 +9,8 @@ const myMakeObsWs = makeObservableWebSocket(
     (url, protocol) => new WebSocket(url, protocol));
 
 connectWampChannel('wss://demo.crossbar.io/ws', 'crossbardemo', undefined, myMakeObsWs).pipe(
-    switchMap(channel => channel.subscribe('io.crossbar.demo.beatbox.692497.pad_down')))
-    .subscribe(([,received]) => console.log('Received: ' + Object.keys(received!)));
+    switchMap(channel => merge(
+        channel.subscribe('io.crossbar.demo.beatbox.692497.pad_down').pipe(map(([, received]) => ({down: received}))),
+        channel.subscribe('io.crossbar.demo.beatbox.692497.pad_up').pipe(map(([, received]) => ({up: received})))
+    )))
+    .subscribe(received => console.log('Received: ' + JSON.stringify(received!)));
