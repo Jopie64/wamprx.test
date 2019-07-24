ae9da2d4a44eee5395669923d93cae108dbfbe11
Johan 't Hart <jopie64@gmail.com>
2019-07-25 00:48:49 +0200

    Using toPromise() and toWampFunc()

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as WebSocket from 'ws';
-import { connectWampChannel, makeObservableWebSocket, makeConsoleLogger, WampChannel } from 'wamprx';
+import { connectWampChannel, makeObservableWebSocket, makeConsoleLogger, WampChannel, toPromise, RegisteredFunc, toWampFunc } from 'wamprx';
 import { switchMap, map, shareReplay, flatMap, take } from 'rxjs/operators';
-import { merge, interval, using, of, never, from } from 'rxjs';
+import { merge, interval, using, of, from } from 'rxjs';
 
 console.log('Play around with beatbox on https://demo.crossbar.io/beatbox/index.html');
 console.log('Use channel 692497');
@@ -16,42 +16,74 @@ const myMakeObsWs = makeObservableWebSocket(
 
 const channel$ = connectWampChannel(
     'wss://demo.crossbar.io/ws', 'crossbardemo', undefined,
-    myMakeObsWs, makeConsoleLogger
+    myMakeObsWs//, makeConsoleLogger
 ).pipe(
     shareReplay({ bufferSize: 1, refCount: true }));
 
-channel$.pipe(
-    switchMap(channel => merge(
-        channel.subscribe('io.crossbar.demo.beatbox.692497.pad_down').pipe(map(([, received]) => ({down: received}))),
-        channel.subscribe('io.crossbar.demo.beatbox.692497.pad_up').pipe(map(([, received]) => ({up: received})))
-    )))
-    .subscribe(received => console.log('Received: ' + JSON.stringify(received!)));
+const subscribeToBeatbox = () => {
+    channel$.pipe(
+        switchMap(channel => merge(
+            channel.subscribe('io.crossbar.demo.beatbox.692497.pad_down').pipe(map(([, received]) => ({down: received}))),
+            channel.subscribe('io.crossbar.demo.beatbox.692497.pad_up').pipe(map(([, received]) => ({up: received})))
+        )))
+        .subscribe(received => console.log('Received: ' + JSON.stringify(received!)));
+};
 
+const beatOnBeatbox = () => {
+    channel$.pipe(
+        switchMap(channel => interval(1000).pipe(
+            switchMap(seq => seq % 2 === 0
+                ? channel.publish('io.crossbar.demo.beatbox.692497.pad_down', [], {b: (seq / 2) % 4, t: 0})
+                : channel.publish('io.crossbar.demo.beatbox.692497.pad_up', [], {b: ((seq - 1) / 2) % 4, t: 0})
+            )
+        ))
+    ).subscribe(pubId => console.log(`Publication id: ${pubId}`));
+};
 
-/*
-channel$.pipe(
-    switchMap(channel => interval(1000).pipe(
-        switchMap(seq => seq % 2 === 0
-            ? channel.publish('io.crossbar.demo.beatbox.692497.pad_down', [], {b: (seq / 2) % 4, t: 0})
-            : channel.publish('io.crossbar.demo.beatbox.692497.pad_up', [], {b: ((seq - 1) / 2) % 4, t: 0})
-        )
-    ))
-).subscribe(pubId => console.log(`Publication id: ${pubId}`));
-*/
+const registerAndCallTestAdd = () => {
+    const callTestAdd = (channel: WampChannel) => interval(1000).pipe(
+        take(10),
+        flatMap(seq =>
+            channel.call('io.crossbar.demo.testAdd', [seq, seq + 1]).pipe(
+                map(([[answer]]) => `${seq} + ${seq + 1} = ${answer}`))
+        ));
 
-const callTestAdd = (channel: WampChannel) => interval(1000).pipe(
-    take(10),
-    flatMap(seq =>
-        channel.call('io.crossbar.demo.testAdd', [seq, seq + 1]).pipe(
-            map(([[answer]]) => `${seq} + ${seq + 1} = ${answer}`))
-    ));
+    channel$.pipe(
+        switchMap(channel =>
+            from(channel.register('io.crossbar.demo.testAdd',
+                toWampFunc((a, b) => of(a + b))
+            )).pipe(
+                flatMap(reg => using(() => reg, _ =>
+                    callTestAdd(channel))))
+        ))
+        .subscribe(
+            answer => console.log(`Sum: ${answer}`),
+            e => console.error('Error: ', e),
+            () => console.log('Complete...'));
+};
 
-channel$.pipe(
-    switchMap(channel =>
-        from(channel.register('io.crossbar.demo.testAdd',
-            ([a, b]) => of([[a + b]])
-        )).pipe(
-            flatMap(reg => using(() => reg, _ =>
-                callTestAdd(channel))))
-    ))
-    .subscribe(answer => console.log(`Sum: ${answer}`));
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const makeImperativeChannel = async () => {
+    const channel = await toPromise(connectWampChannel(
+            'wss://demo.crossbar.io/ws', 'crossbardemo', undefined,
+            myMakeObsWs, makeConsoleLogger
+        ));
+    try
+    {
+        await sleep(1000);
+        
+        const [[answer]] = await channel.call('io.crossbar.demo.testAdd', [100, 100]).toPromise();
+
+        console.log('Answer:', answer);
+    } finally {
+        channel.unsubscribe();
+    }
+}
+
+subscribeToBeatbox();
+// beatOnBeatbox();
+registerAndCallTestAdd();
+makeImperativeChannel()
+    .then(_ => console.log('Imperative thing done'))
+    .catch(e => console.error('Imperative thing failed', e));
