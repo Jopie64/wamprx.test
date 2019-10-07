3a37d5dbfeb8ce94f420e957265719ce6c2176fd
Johan 't Hart <johan.thart@peterconnects.com>
2019-10-08 01:39:03 +0200

    LoadTest: Measure individual times

--- src/loadTest.ts
+++ src/loadTest.ts
@@ -1,6 +1,6 @@
 import { makeChannel$, runWithChannel } from './channel'
-import { mergeMap, map } from 'rxjs/operators';
-import { of, from, using, combineLatest } from 'rxjs';
+import { mergeMap, map, shareReplay, take } from 'rxjs/operators';
+import { of, from, using, combineLatest, NEVER, concat } from 'rxjs';
 import { WampChannel } from 'wamprx';
 import { range } from 'ramda';
 
@@ -11,20 +11,37 @@ export const runLoadTest = () => runWithChannel(channel$, runLoadTestWithChannel
 var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'; //0123456789';
 
 const makeRandomName = (length: number) =>
-    range(0, length).reduce((a, c) => a + characters.charAt(Math.floor(Math.random() * characters.length)), '')
+    range(0, length).reduce((a, c) => a + characters.charAt(Math.floor(Math.random() * characters.length)), '');
+
+const printTime = (t: bigint) =>
+    Number(t / 1000000n) / 1000;
 
 const runLoadTestWithChannel = async (channel: WampChannel) => {
     //const fnames = range(0, 2000).map(n => `greetMe${n}`);
     const fnames = range(0, 2000).map(n => makeRandomName(500));
     console.log('Connected. Begin test...', fnames[0]);
-    const start = process.hrtime();
-    const result = await combineLatest(
-        fnames.map(fname => from(channel.register(fname, ([name]) => of([[`Hello ${name}!`]]))).pipe(
-            mergeMap(r => using(() => r, _ => channel.call(fname, ['Johan']))),
-            map(([[result]]) => result === 'Hello Johan!'))
+
+    console.log('Registering...');
+    const start = process.hrtime.bigint();
+    const registrations$ = combineLatest(
+        fnames.map(fname => from(channel.register(fname, ([name]: any) => of([[`Hello ${name}!`]]))).pipe(
+            mergeMap(r => using(() => r, _ => concat(of(true), NEVER)))
         ))
-        .toPromise();
-    const duration = process.hrtime(start);
-    console.log(`Result ${(duration[0] + duration[1] / 1000000000).toFixed(3)}s`, result.length, result.reduce((a, c) => a && c));
-    console.log('Done');
+    ).pipe(shareReplay({bufferSize: 1, refCount: true }));
+    const registrations = registrations$.subscribe();
+    const regResult = await registrations$.pipe(take(1)).toPromise();
+
+    const registeredTs = process.hrtime.bigint();
+
+    console.log(`Registered ${regResult.length} functions in ${printTime(registeredTs - start)}s. Calling...`);
+    const result = await combineLatest(fnames.map(
+        fname => channel.call(fname, ['Johan']).pipe(
+            map(([[result]]: any) => result === 'Hello Johan!'))
+    )).toPromise();
+
+    const calledTs = process.hrtime.bigint();
+    console.log(`Called in ${printTime(calledTs - registeredTs)}s Unregistering...`);
+    registrations.unsubscribe();
+    const duration = process.hrtime.bigint() - start;
+    console.log(`Result ${printTime(duration)}s`, result.length, result.reduce((a, c) => a && c));
 }
